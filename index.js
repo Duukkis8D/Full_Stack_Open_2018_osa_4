620914813929057374b69f8ab4ab9528dea5ac93
Tuukka Virtanen <duukkis@gmail.com>
2018-11-16 04:45:30 +0200

    Tehtävä 4.2 valmis

--- index.js
+++ index.js
@@ -4,18 +4,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const Blog = mongoose.model('Blog', {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-});
-
-module.exports = Blog;
+const blogsRouter = require('./controllers/blogs');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/api/blogs', blogsRouter);
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -29,24 +22,6 @@ mongoose
     .catch( err => {
         console.log(err);
     });
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs);
-        });
-});
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        });
-});
 
 const PORT = 3003;
 app.listen(PORT, () => {
