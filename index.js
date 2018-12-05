3434693b6bbf53d6041e9f1ac76244db4d8d8a95
Tuukka Virtanen <duukkis@gmail.com>
2018-12-05 04:46:07 +0200

    Tehtävä 4.18 valmis

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const config = require('./utils/config');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 mongoose
     .connect(config.mongolabUri, { useNewUrlParser: true })
